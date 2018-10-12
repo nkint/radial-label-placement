65e036f8b24e2b6018fa18107044430018cfcb17
alberto <alberto.massa@ymail.com>
2018-10-12 18:34:04 +0200

    moved test to typescript

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,7 @@
 import test from 'ava'
 import { radialLabelPlacement } from '../src'
 
-test(t => {
+test('common case', t => {
   const expected = [
     { textAlign: 'start', textBaseline: 'middle' },
     { textAlign: 'start', textBaseline: 'top' },
@@ -11,7 +11,7 @@ test(t => {
     { textAlign: 'end', textBaseline: 'bottom' },
     { textAlign: 'center', textBaseline: 'bottom' },
     { textAlign: 'start', textBaseline: 'bottom' },
-    { textAlign: 'start', textBaseline: 'bottom' },
+    { textAlign: 'start', textBaseline: 'middle' },
   ]
 
   const input = [0, 1 / 4, 1 / 2, 3 / 4, 1, 5 / 4, 3 / 2, 7 / 4, 2].map(x => x * Math.PI)
@@ -19,4 +19,21 @@ test(t => {
   const output = input.map(radialLabelPlacement)
 
   t.deepEqual(expected, output)
+})
+
+test('Math.PI * 2 - 0.001', t => {
+  const expected = { textAlign: 'start', textBaseline: 'bottom' }
+  const input = Math.PI * 2 - 0.1
+  const output = radialLabelPlacement(input)
+  t.deepEqual(expected, output)
+})
+
+test('More then Math.PI * 2', t => {
+  const expected = { textAlign: 'start', textBaseline: 'middle' }
+  const input = [0.1, Math.PI * 2 + 0.1]
+
+  input.forEach(i => {
+    const output0 = radialLabelPlacement(i)
+    t.deepEqual(expected, output0)
+  })
 })
